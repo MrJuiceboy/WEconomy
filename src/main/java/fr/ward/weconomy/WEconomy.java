b7c73259df138d4318781217105465e096afb98b
MrJuiceboy <juiceboymail@gmail.com>
2023-02-01 13:27:39 +0100

    Initial commit
    Initial commit

---
+++ src/main/java/fr/ward/weconomy/WEconomy.java
@@ -0,0 +1,21 @@
+package fr.ward.weconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WEconomy extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
